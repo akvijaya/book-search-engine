11988c0a0a49c5d88c27d11c67edda1a11f1c114
akvijaya <abisonvijay@gmail.com>
2022-01-18 19:52:41 -0500

    dnfjs
    hhh
    
    fdfdf
    
    ~done now
    git commit -m "done"
    
    insert

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
+const startServer = async () => {
   // create a new Apollo server and pass in our schema data
   const server = new ApolloServer({ 
     typeDefs, 
@@ -20,7 +20,7 @@ const app = express();
     context: authMiddleware
   });
 
-  
+  await server.start()
 
   // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
@@ -28,7 +28,7 @@ const app = express();
   // log where we can go to test our GQL API
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 
-
+};
 
 
 app.use(express.urlencoded({ extended: false }));
